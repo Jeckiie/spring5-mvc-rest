d50b14ec7d4adba9df07e4d98de017d793834f96
jerkokosic <jerkokosic1@gmail.com>
2021-04-27 12:37:29 +0200

    added mapper for category

--- src/main/java/guru/springfamework/api/v1/model/CategoryDTO.java
+++ src/main/java/guru/springfamework/api/v1/model/CategoryDTO.java
@@ -1,8 +1,13 @@
 package guru.springfamework.api.v1.model;
 
+import guru.springfamework.api.v1.mapper.CategoryMapper;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+
 /**
  * Created by jt on 9/24/17.
  */
+@Data
 public class CategoryDTO {
     private Long id;
     private String name;
