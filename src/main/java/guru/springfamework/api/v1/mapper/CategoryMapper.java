65f6a2407fe12775d103c2892fb6fd53cbe6efbc
jerkokosic <jerkokosic1@gmail.com>
2021-04-27 13:13:00 +0200

    added category service and controller

--- src/main/java/guru/springfamework/api/v1/mapper/CategoryMapper.java
+++ src/main/java/guru/springfamework/api/v1/mapper/CategoryMapper.java
@@ -2,14 +2,14 @@ package guru.springfamework.api.v1.mapper;
 
 import guru.springfamework.api.v1.model.CategoryDTO;
 import guru.springfamework.domain.Category;
+import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface CategoryMapper {
 
-    CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
+  //  CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
 
     @Mapping(source = "id", target = "id")
     CategoryDTO categoryToCategoryDTO(Category category);
